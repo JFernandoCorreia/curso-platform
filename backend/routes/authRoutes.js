7e4c30f73ee790888b24e4652ead6a7aaf4cde2e
JFernando <almo_fernando@hotmail.com>
2023-12-05 13:57:38 -0300

    novos commits

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const bcrypt = require('bcrypt');
+const { User } = require('../models');
 
 const router = express.Router();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+
+User.addHook('beforeCreate', async (user, options) => {
+  const salt = await bcrypt.genSalt();
+  user.password = await bcrypt.hash(user.password, salt);
+});
 
 module.exports = router;
--- backend/routes/cursoRoutes.js
+++ backend/routes/cursoRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const autenticar = require('../middleware/authMiddleware');
 const cursoController = require('../controllers/cursoController');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 const router = express.Router();
 
 router.get('/', autenticar, cursoController.listarCursos);
-router.post('/cadastrar', autenticar, cursoController.cadastrarCurso);
+router.post('/cadastrar', autenticar, upload.single('imagem'), cursoController.cadastrarCurso);
 router.put('/:id/editar', autenticar, cursoController.editarCurso);
 router.get('/:id/buscar', autenticar, cursoController.buscarCurso);
 router.delete('/:id/deletar', autenticar, cursoController.deletarCurso);
