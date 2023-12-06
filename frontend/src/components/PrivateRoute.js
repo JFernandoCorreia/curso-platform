e5ee6a24aa13b833a3a29660ebb4fa4c10159b84
JFernando <almo_fernando@hotmail.com>
2023-12-06 07:00:58 -0300

    json

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import Cursos from './Cursos';
 
 const PrivateRoute = ({ element: Element, ...rest }) => {
   const usuarioAutenticado = localStorage.getItem('token');
