00d82b49c73c3484c5658108c17e2d983acef808
JFernando <almo_fernando@hotmail.com>
2023-12-06 07:39:52 -0300

    json2

--- frontend/src/components/CadastroUsuario.js
+++ frontend/src/components/CadastroUsuario.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-const [feedback, setFeedback] = useState('');
 
 
 const CadastroUsuario = ({ history }) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [feedback, setFeedback] = useState('');
 
   const handleCadastro = async () => {
     try {
